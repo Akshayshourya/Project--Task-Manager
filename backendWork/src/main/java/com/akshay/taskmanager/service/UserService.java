6453377e7b7fd31dab4c616a89c82a7603de985e
Akshayshourya <krupajain703@gmail.com>
2026-03-26 14:26:11 +0530

    Method for retrieving all users (for admins only)

--- backendWork/src/main/java/com/akshay/taskmanager/service/UserService.java
+++ backendWork/src/main/java/com/akshay/taskmanager/service/UserService.java
@@ -2,6 +2,8 @@ package com.akshay.taskmanager.service;
 
 import com.akshay.taskmanager.dto.LoginRequest;
 import com.akshay.taskmanager.dto.UserRequest;
+import com.akshay.taskmanager.dto.UserResponse;
+import com.akshay.taskmanager.entity.Role;
 import com.akshay.taskmanager.entity.User;
 import com.akshay.taskmanager.exception.InvalidCredentialsException;
 import com.akshay.taskmanager.repository.UserRepository;
@@ -11,6 +13,8 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -27,6 +31,7 @@ public class UserService {
         user.setEmail(request.getEmail());
         user.setName(request.getName());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(Role.USER);
 
         return userRepository.save(user);
     }
@@ -41,5 +46,15 @@ public class UserService {
             throw new InvalidCredentialsException();
 
         return JwtUtil.generateToken(user.getId(), user.getEmail());
+    }
+
+    public List<UserResponse> getAllUsers(){
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new UserResponse(user.getId(),
+                        user.getEmail(),
+                        user.getName(),
+                        user.getRole()))
+                .toList();
     }
 }
