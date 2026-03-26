d9f1c75d87459d834e481f272c0b5e31483b0904
Akshayshourya <krupajain703@gmail.com>
2026-03-26 14:23:24 +0530

    Method for retrieving all users (for admins only)

--- backendWork/src/main/java/com/akshay/taskmanager/controller/UserController.java
+++ backendWork/src/main/java/com/akshay/taskmanager/controller/UserController.java
@@ -3,15 +3,17 @@ package com.akshay.taskmanager.controller;
 
 import com.akshay.taskmanager.dto.LoginRequest;
 import com.akshay.taskmanager.dto.UserRequest;
+import com.akshay.taskmanager.dto.UserResponse;
+import com.akshay.taskmanager.entity.User;
 import com.akshay.taskmanager.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(("/users"))
@@ -35,4 +37,11 @@ public class UserController {
         String token = userService.login(request);
         return ResponseEntity.ok(token);
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    List<UserResponse> getAllUsers (){
+        return userService.getAllUsers();
+    }
+
 }
